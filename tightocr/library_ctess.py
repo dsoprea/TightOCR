e4f974018e8468cb9bb687f349d567acf96af20a
Dustin Oprea <myselfasunder@gmail.com>
2014-01-26 17:14:51 -0500

    Now fallback to default library names.

--- tightocr/library_ctess.py
+++ tightocr/library_ctess.py
@@ -2,5 +2,8 @@ from ctypes import cdll
 from ctypes.util import find_library
 
 _TESS_FILEPATH = find_library('ctesseract')
+if _TESS_FILEPATH is None:
+    _TESS_FILEPATH = 'libctesseract.so'
+
 libctess = cdll.LoadLibrary(_TESS_FILEPATH)
 
--- tightocr/library_lept.py
+++ tightocr/library_lept.py
@@ -2,5 +2,8 @@ from ctypes import cdll
 from ctypes.util import find_library
 
 _LEPT_FILEPATH = find_library('lept')
+if _LEPT_FILEPATH is None:
+    _LEPT_FILEPATH = 'liblept.so'
+
 liblept = cdll.LoadLibrary(_LEPT_FILEPATH)
 
